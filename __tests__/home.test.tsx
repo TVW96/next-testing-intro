8bcd7c826ca6da4191f7cd24bbf63190005a3c67
Troy Washington <troyvw96@gmail.com>
2025-02-01 19:34:45 -0800

    created test suite and 2 additional tests

--- __tests__/home.test.tsx
+++ __tests__/home.test.tsx
@@ -1,11 +1,34 @@
 import { render, screen } from '@testing-library/react'
 import Home from '@/app/page'
+describe('Home', () => {
+    it('should have a link to Next.js Docs', () => {
+        render(<Home />) // A - ARRANGE
 
-it('should have a link to Next.js Docs', () => {
-    render(<Home />) // A - ARRANGE
+        const link = screen.getByRole('link', { name: /docs/i }) // A - ACT
 
-    const link = screen.getByRole('link', { name: /docs/i }) // A - ACT
+        expect(link).toBeInTheDocument() // A - ASSERT
+        expect(link).toHaveAttribute('href', expect.stringContaining('docs')) // Ensure href contains 'docs'
+    })
 
-    expect(link).toBeInTheDocument() // A - ASSERT
-    expect(link).toHaveAttribute('href', expect.stringContaining('docs')) // Ensure href contains 'docs'
+    it('should contain a link to Vercel Docs', () => {
+        render(<Home />) // A - ARRANGE
+
+        const link = screen.getByRole('link', {
+            name: /Vercel/i
+        }) // A - ACT
+
+        expect(link).toBeInTheDocument() // A - ASSERT
+        expect(link).toHaveAttribute('href', expect.stringContaining('vercel')) // Ensure href contains 'docs'
+    })
+
+    it('should have a heading', () => {
+        render(<Home />) // A - ARRANGE
+
+        const myElem = screen.getByRole('link', {
+            name: "Learn"
+        }) // A - ACT
+
+        expect(myElem).toBeInTheDocument() // A - ASSERT
+    })
 })
+
